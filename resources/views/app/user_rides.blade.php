408c6912c84e87b322f519a0775ff1f5f4dd2e33
R1M <get.r1m.in@gmail.com>
2026-04-17 10:55:57 +0530

    Update #20260417-1055

--- resources/views/app/user_rides.blade.php
+++ resources/views/app/user_rides.blade.php
@@ -3,6 +3,20 @@
     <x-user-layout :$user>
 
 
+        <table>
+            <tbody>
+                @foreach ($rides as $ride)
+                    <tr>
+                        <td>{{ $ride->request->from_location }}</td>
+                        <td>{{ $ride->request->to_location }}</td>
+                        <td>{{ $ride->status }}</td> 
+                        <td>{{ $ride->distance }}</td> 
+                        <td>{{ $ride->duration }}</td> 
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
         @dump($rides)
 
         @dump($requests)
