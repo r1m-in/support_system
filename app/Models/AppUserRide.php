6f9006b2b63afc86285d56d749e02217287f0e17
R1M <get.r1m.in@gmail.com>
2026-04-17 10:50:12 +0530

    Update #20260417-1050

--- app/Models/AppUserRide.php
+++ app/Models/AppUserRide.php
@@ -12,7 +12,9 @@ class AppUserRide extends Model
 
    protected $guarded = ['*'];
 
-   // app_user_id, name, email, phone, referred_by 
-   // status, provider, created_by, created_at, updated_by, updated_at 
+   public function request()
+   {
+      return $this->belongsTo(AppCity::class, 'request_id', 'id');
+   }
 
 }
