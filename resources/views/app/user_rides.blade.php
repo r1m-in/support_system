459329adb8e068f6cd7c0752f5d60f54b75768bd
R1M <get.r1m.in@gmail.com>
2026-04-17 11:12:29 +0530

    Update #20260417-1112

--- resources/views/app/user_rides.blade.php
+++ resources/views/app/user_rides.blade.php
@@ -5,26 +5,34 @@
         <div class="card">
             <div class="card-body">
 
-                <table>
-                    <tbody>
-                        @foreach ($rides as $ride)
-                            <tr>
-                                <td>{{ $ride->request->from_location }}</td>
-                                <td>{{ $ride->request->to_location }}</td>
-                                <td>{{ $ride->request->status }}</td>
-                                <td>Reason: {{ $ride->request->reason }}</td>
-                                <td>Distance: {{ $ride->request->distance }}</td>
-                                <td>Duration: {{ $ride->request->duration }}</td>
-                                <td>
-                                    @json($ride->request)
-                                </td>
-                                <td>
-                                    @json($ride)
-                                </td>
+                <div class="table-responsive">
+                    <table class="table align-middle gs-0 gy-4 text-center table-hover">
+                        <thead>
+                            <tr class="fw-bolder text-white bg-dark">
+                                <th class="min-w-40px rounded-start text-center">From</th>
+                                <th class="min-w-125px">To</th>
+                                <th class="min-w-125px">Status</th>
+                                <th class="min-w-125px">Reason</th>
+                                <th class="min-w-125px text-center">Distance</th>
+                                <th class="min-w-100px text-center rounded-end">Duration</th>
                             </tr>
-                        @endforeach
+                        </thead>
+                        <tbody>
+                            @foreach ($rides as $ride)
+                                <tr>
+                                    <td>{{ $ride->request->from_location }}</td>
+                                    <td>{{ $ride->request->to_location }}</td>
+                                    <td>{{ $ride->request->status }}</td>
+                                    <td>{{ $ride->request->reason }}</td>
+                                    <td>{{ $ride->request->distance }}</td>
+                                    <td>{{ $ride->request->duration }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                     </tbody>
-                </table>
+                    </table>
+                </div>
 
             </div>
         </div>
