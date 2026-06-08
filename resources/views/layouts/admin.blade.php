15a8b4646eaa708eb0eb98270d6f289250d96115
R1M <get.r1m.in@gmail.com>
2026-06-08 16:31:01 +0530

    Update #20260608-1630

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -169,28 +169,28 @@
                             </div>
 
 
-                            <div class="menu-item">
-                                <a class="menu-link @if (request()->routeIs('reasons')) active @endif"
-                                    href="{{ route('reasons') }}">
-                                    <span class="menu-icon">
-                                        <span class="svg-icon svg-icon-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                viewBox="0 0 24 24" fill="none">
-                                                <rect opacity="0.3" x="2" y="2" width="20" height="20"
-                                                    rx="10" fill="currentColor" />
-                                                <rect x="11" y="17" width="7" height="2" rx="1"
-                                                    transform="rotate(-90 11 17)" fill="currentColor" />
-                                                <rect x="11" y="9" width="2" height="2" rx="1"
-                                                    transform="rotate(-90 11 9)" fill="currentColor" />
-                                            </svg>
+                            @role(\App\Enums\User\RoleEnum::ADMIN)
+                                <div class="menu-item">
+                                    <a class="menu-link @if (request()->routeIs('reasons')) active @endif"
+                                        href="{{ route('reasons') }}">
+                                        <span class="menu-icon">
+                                            <span class="svg-icon svg-icon-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none">
+                                                    <rect opacity="0.3" x="2" y="2" width="20" height="20"
+                                                        rx="10" fill="currentColor" />
+                                                    <rect x="11" y="17" width="7" height="2" rx="1"
+                                                        transform="rotate(-90 11 17)" fill="currentColor" />
+                                                    <rect x="11" y="9" width="2" height="2" rx="1"
+                                                        transform="rotate(-90 11 9)" fill="currentColor" />
+                                                </svg>
+                                            </span>
                                         </span>
-                                    </span>
-                                    <span class="menu-title">Reasons</span>
-                                </a>
-                            </div>
+                                        <span class="menu-title">Reasons</span>
+                                    </a>
+                                </div>
 
 
-                            @can(\App\Enums\User\PermissionEnum::USER_VIEW)
                                 <div data-kt-menu-trigger="click"
                                     class="menu-item  @if (request()->routeIs('user.*') || request()->routeIs('users')) here show @endif menu-accordion">
                                     <span class="menu-link">
@@ -221,20 +221,18 @@
                                             </a>
                                         </div>
 
-                                        @role(\App\Enums\User\RoleEnum::ADMIN)
-                                            <div class="menu-item">
-                                                <a class="menu-link @if (request()->routeIs('user.roles')) active @endif"
-                                                    href="{{ route('user.roles') }}">
-                                                    <span class="menu-bullet">
-                                                        <span class="bullet bullet-dot"></span>
-                                                    </span>
-                                                    <span class="menu-title">Roles</span>
-                                                </a>
-                                            </div>
-                                        @endrole
+                                        <div class="menu-item">
+                                            <a class="menu-link @if (request()->routeIs('user.roles')) active @endif"
+                                                href="{{ route('user.roles') }}">
+                                                <span class="menu-bullet">
+                                                    <span class="bullet bullet-dot"></span>
+                                                </span>
+                                                <span class="menu-title">Roles</span>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
-                            @endcan
+                            @endrole
 
 
                             <div class="menu-item">
@@ -258,8 +256,6 @@
                                     <span class="menu-title">Profile</span>
                                 </a>
                             </div>
-
-
 
                         </div>
                     </div>
