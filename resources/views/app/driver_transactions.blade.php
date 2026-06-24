58cc032a012cd892aa4abbfdaf7fb911b69e22b0
R1M <get.r1m.in@gmail.com>
2026-06-24 22:55:28 +0530

    Update #20260624-2255

--- resources/views/app/driver_transactions.blade.php
+++ resources/views/app/driver_transactions.blade.php
@@ -10,13 +10,10 @@
                     <table class="table align-middle gs-0 gy-4 text-center table-hover">
                         <thead>
                             <tr class="fw-bolder text-white bg-dark">
-                                <th class="min-w-125px rounded-start ps-2">From</th>
-                                <th class="min-w-125px">To</th>
-                                <th class="min-w-125px">Created</th>
-                                <th class="min-w-50px">Status</th>
-                                <th class="min-w-125px">User Name</th>
-                                <th class="min-w-50px">Fare</th>
-                                <th class="min-w-150px rounded-end pe-2">Actions</th>
+                                <th class="min-w-100px rounded-start ps-2">Type</th>
+                                <th class="min-w-100px">Date</th>
+                                <th class="min-w-150px">Description</th> 
+                                <th class="min-w-80px rounded-end pe-2">Amount</th>
                             </tr>
                         </thead>
                         <tbody>
