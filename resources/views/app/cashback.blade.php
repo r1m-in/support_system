865924a2213633994420408bcadbe420e9bd69c2
R1M <get.r1m.in@gmail.com>
2026-07-10 17:03:33 +0530

    Update #20260710-1703

--- resources/views/app/cashback.blade.php
+++ resources/views/app/cashback.blade.php
@@ -21,7 +21,7 @@
                             <tr>
                                 <td>{{ date('dS M Y h:i a', strtotime($item['created_at'])) }}</td>
                                 <td>{{ $item['app_driver_id'] }}</td>
-                                <td>{{ $item['amount'] }}</td>
+                                <td>{{ $item['balance'] }}</td>
                             </tr>
                         @empty
                             <tr>
