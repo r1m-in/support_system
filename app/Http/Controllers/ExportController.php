299e5e013fe9e85f55bf0516e66a32e5b21dca60
R1M <get.r1m.in@gmail.com>
2026-09-04 23:09:19 +0530

    Update #20260904-2309

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -20,21 +20,25 @@ class ExportController extends Controller
          $from = Carbon::parse($request->from)->toDateString();
          $to = Carbon::parse($request->to)->toDateString();
          $drivers = AppDriver::with('roles')->latest()->whereBetween('created_at', [$from, $to]);
+
+         $keyword = $data['search'];
+
+         if ($keyword) {
+            $drivers->where(function ($query) use ($keyword) {
+               $query->where('name', 'LIKE', "%$keyword%")
+                  ->orWhere('app_driver_id', 'LIKE', "%$keyword%")
+                  ->orWhere('phone', 'LIKE', "%$keyword%");
+            })->whereHas('roles', function ($query) {
+               $query->where('role_id', '4b99bc3a-13bc-11f0-a1a1-0a74e7f1ccd1');
+            });
+         }
+
+         $data['drivers']  = $drivers->get();
+      } else {
+         $data['drivers']  = $drivers;
       }
 
-      $keyword = $data['search'];
 
-      if ($keyword) {
-         $drivers->where(function ($query) use ($keyword) {
-            $query->where('name', 'LIKE', "%$keyword%")
-               ->orWhere('app_driver_id', 'LIKE', "%$keyword%")
-               ->orWhere('phone', 'LIKE', "%$keyword%");
-         })->whereHas('roles', function ($query) {
-            $query->where('role_id', '4b99bc3a-13bc-11f0-a1a1-0a74e7f1ccd1');
-         });
-      }
-
-      $data['drivers']  = $drivers->get();
 
       return view('export.owners', $data);
    }
